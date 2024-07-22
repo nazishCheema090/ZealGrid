b82d64733b7d6ec77495d4bbf2a3d59810d2c588
Tamoor Chatha <tamoorchatha82@gmail.com>
2024-07-23 02:32:43 +0500

    WIP: on home page for add Project Action

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,17 +14,52 @@ const Home = () => {
       id: 1,
       name: "Add Project",
       description: "",
-      isAddProject: true
+      isAddProject: true,
     },
     {
       id: 2,
       name: "Netflix",
-      description: "Netflix website 7fasd5f6"
+      description: "Netflix website 7fasd5f6",
     },
     {
       id: 3,
-      name: "Netflix",
-      description: "Netflix website 7fasd5f6"
+      name: "Spotify",
+      description: "Spotify clone project",
+    },
+    {
+      id: 4,
+      name: "Amazon",
+      description: "Amazon website clone",
+    },
+    {
+      id: 5,
+      name: "Google",
+      description: "Google search clone",
+    },
+    {
+      id: 6,
+      name: "Facebook",
+      description: "Facebook clone",
+    },
+    {
+      id: 7,
+      name: "Twitter",
+      description: "Twitter clone project",
+    },
+    {
+      id: 8,
+      name: "LinkedIn",
+      description: "LinkedIn clone",
+    },
+    {
+      id: 9,
+      name: "YouTube",
+      description: "YouTube clone project",
+    },
+    {
+      id: 10,
+      name: "Instagram",
+      description: "Instagram clone project",
     },
   ];
 
