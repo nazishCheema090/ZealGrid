ef5546471b3945ab2f32cecd9354fc9141770e33
haseeb491 <uhaseeb348@gmail.com>
2024-09-10 19:58:02 +0500

    sidebar-UI

--- src/components/sidebar/page.jsx
+++ src/components/sidebar/page.jsx
@@ -6,79 +6,123 @@ import {
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import AppsIcon from "@mui/icons-material/Apps";
-import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from "@mui/icons-material/Logout";
 import { useParams, useNavigate } from "react-router-dom";
+import {
+  toggles,
+  app,
+  labels,
+  navigation,
+  overview,
+  settings,
+  logo,
+} from "../../constants/sidebarIcons";
 
-const menuItems = [
+const sidebarItems = [
   {
     text: "overview",
-    icon: <DashboardIcon />,
+    icon: (
+      <img
+        src={overview}
+        alt="overview icon"
+        className="w-6 h-6 cursor-pointer"
+      />
+    ),
   },
   {
     text: "app",
-    icon: <AppsIcon />,
+    icon: <img src={app} alt="app icon" className="w-6 h-8 cursor-pointer" />,
   },
   {
     text: "navigation",
-    icon: <AppsIcon />,
+    icon: (
+      <img
+        src={navigation}
+        alt="navigation icon"
+        className="w-6 h-6 cursor-pointer"
+      />
+    ),
   },
   {
     text: "labels",
-    icon: <AppsIcon />,
+    icon: (
+      <img src={labels} alt="labels icon" className="w-6 h-6 cursor-pointer" />
+    ),
   },
   {
     text: "toggles",
-    icon: <AppsIcon />,
+    icon: (
+      <img
+        src={toggles}
+        alt="toggles icon"
+        className="w-10 h-6 cursor-pointer"
+      />
+    ),
   },
   {
     text: "settings",
-    icon: <SettingsIcon />,
+    icon: (
+      <img
+        src={settings}
+        alt="overview icon"
+        className="w-6 h-6 cursor-pointer"
+      />
+    ),
   },
-  { text: "logout", icon: <LogoutIcon />, route: "#" },
 ];
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const { projectName } = useParams();
-  const [activeItem, setActiveItem] = useState("Overview");
+  const [activeItem, setActiveItem] = useState("overview");
 
   return (
-    <div className="bg-gray-800 text-white h-screen w-64">
-      <div className="flex items-center justify-center py-6">
-        <h1
-          className="text-2xl font-bold cursor-pointer "
+    <div className="bg-gray-800 text-white h-screen w-[100px] sm:w-64">
+      <div className="flex items-center justify-center pt-[35px] pb-[70px] ">
+        <img
+          src={logo}
+          className="w-40 h-8 cursor-pointer hidden sm:block"
+          alt="Zealgrid logo"
           onClick={() => navigate("/")}
-        >
-          ZealGrid
-        </h1>
+        />
       </div>
-      <List>
-        {menuItems.map((item) => (
-          <ListItem key={item.text} disablePadding>
-            <ListItemButton
-              className={`hover:bg-gray-700 ${
-                activeItem === item.text && "bg-blue-600"
-              }`}
-              onClick={() => {
-                setActiveItem(item.text);
-                if (item.text == "overview") {
-                  navigate(`/project-details/${projectName}`);
-                } else if (item.text == "logout") {
-                  navigate("#");
-                } else {
-                  navigate(`/project-details/${projectName}/${item.text}`);
-                }
-              }}
-            >
-              <ListItemIcon className="text-white">{item.icon}</ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <div className="text-gray-200 px-6 ">
+        <span className="text-gray-300 hidden sm:block ">Dashboard</span>
+      </div>
+      <div className="px-4">
+        <List className="flex flex-col justify-center">
+          {sidebarItems.map((item) => (
+            <ListItem key={item.text} disablePadding className="pb-1">
+              <ListItemButton
+                sx={{
+                  background:
+                    activeItem === item.text
+                      ? "linear-gradient(90deg, #175CFF 0%, rgba(23, 92, 255, 0.2) 81.79%)"
+                      : "inherit",
+                  "&:hover": {
+                    background:
+                      "linear-gradient(90deg, #175CFF 0%, rgba(23, 92, 255, 0.2) 81.79%)",
+                  },
+                  borderRadius: "10px",
+                }}
+                onClick={() => {
+                  setActiveItem(item.text);
+                  if (item.text == "overview") {
+                    navigate(`/project-details/${projectName}`);
+                  } else {
+                    navigate(`/project-details/${projectName}/${item.text}`);
+                  }
+                }}
+              >
+                <ListItemIcon className="">{item.icon}</ListItemIcon>
+                <ListItemText
+                  primary={item.text}
+                  className="text-gray-300 hidden sm:block"
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
 };
---
+++ src/constants/sidebarIcons.js
@@ -0,0 +1,9 @@
+import overview from "../assets/overview.png";
+import app from "../assets/app.png";
+import toggles from "../assets/toggles.png";
+import labels from "../assets/labels.png";
+import navigation from "../assets/navigation.png";
+import settings from "../assets/settings.png";
+import logo from "../assets/logo.png";
+
+export { overview, app, toggles, labels, navigation, settings, logo };
